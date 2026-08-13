54b69757c39ce8cd567e1ce97174e24c3d60dc75
kamrangamer866 <kamrangamer866@gmail.com>
2026-08-13 23:47:35 +0500

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,8 @@
+const SUPABASE_URL = "https://dldavzzdcabybdeyxyuz.supabase.co";
 
+const SUPABASE_KEY = "YAHAN_APNI_SB_PUBLISHABLE_KEY_PASTE_KARO";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
