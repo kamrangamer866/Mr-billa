7a9a014a94739b54c432d0952f08b0654de6beb1
kamrangamer866 <kamrangamer866@gmail.com>
2026-08-13 15:34:02 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
 function welcome() {
-    document.querySelector(".card").innerHTML = `
-        <h1>🎥 MR BILLA VIDEO</h1>
-
-        <video width="100%" controls autoplay>
-            <source src="VID-20260804-WA0001.mp4" type="video/mp4">
-            Your browser does not support the video tag.
-        </video>
-
-        <br><br>
-
-        <a href="https://wa.me/03326657632" target="_blank">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" width="60">
-        </a>
-    `;
+    document.getElementById("myVideo").style.display = "block";
+    document.getElementById("myVideo").play();
 }
