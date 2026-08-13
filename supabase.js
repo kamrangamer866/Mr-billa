619a6fedbf367d186e45d46b6412d76d1b2c55e4
kamrangamer866 <kamrangamer866@gmail.com>
2026-08-14 02:46:17 +0500

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,11 +1,8 @@
-const SUPABASE_URL =
-    "https://dldavzzdcabybdeyxyuz.supabase.co";
+const SUPABASE_URL = "https://dldavzzdcabybdeyxyuz.supabase.co";
 
-const SUPABASE_KEY =
-    "sb_publishable_IyuJvmSMXxH0g_hLG1mNrg_HmHzSdcM";
+const SUPABASE_KEY = "sb_publishable_IyuJvmSMXxH0g_hLG1mNrg_HmHzSdcM";
 
-const supabaseClient =
-    window.supabase.createClient(
-        SUPABASE_URL,
-        SUPABASE_KEY
-    );
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
